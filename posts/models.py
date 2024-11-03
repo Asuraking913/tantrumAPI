ee53c6a35abd0c422f992fbbd52ee4d2bd3e46d3
Israel Shedrack <israelshedrack913@gmail.com>
2024-11-03 12:11:12 +0100

    feat: created models, db init

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from .utils import generate_id
+
 
 # Create your models here.
+class Posts(models.Model):
+    id = models.CharField(
+        max_length=255, primary_key=True, null=False, blank=False, default=generate_id
+    )
+    post_email = models.EmailField()
+    time = models.DateTimeField(auto_now_add=True)
+
+
+class Comments(models.Model):
+    id = models.CharField(
+        max_length=255, primary_key=True, null=False, blank=False, default=generate_id
+    )
+    comment_email = models.EmailField()
+    time = models.DateTimeField(auto_now_add=True)
